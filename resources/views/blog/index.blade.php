82e2a5fc071d531d7c91d3a6f90d291acf7dc771
anas sadouq <sadouqanas1@gmail.com>
2024-02-03 22:49:38 +0100

    change website texts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -22,9 +22,12 @@
             <img src="https://picsum.photos/id/240/460/400" alt="">
         </div>
         <div class="col">
-            <h2 class="font-bold" style="font-weight : bold; font-size:30px">HOW TO BE AN EXPERT IN 2023</h2>
-            <p style="font-weight : bold" >You can fixe your all your programming language here</p>
-            <p class="text-secondary">Windows talking painted pasture yet its express parties use. Sure last upon he same as knew next. Of believed or diverted no rejoiced. End friendship sufficient assistance can prosperous met. As game he show it park do. Was has unknown few certain ten promise. No finished my an likewise cheerful packages we. For assurance concluded son something depending discourse see led collected. Packages oh no denoting my advanced humoured. Pressed be so thought natural.</p>
+            <h2 class="font-bold" style="font-weight : bold; font-size:30px">CREATE YOUR BLOG</h2>
+            <p style="font-weight : bold" >A blog project typically involves creating and managing blogs</p>
+            <p class="text-secondary">This means that a blog project starts with the creation and ongoing management of a blog. A blog is an online platform, or website, where content is regularly published. 
+                which is an online platform where individuals or organizations can share information, thoughts, and updates in the form of articles or posts.
+                It serves as an online space where people, whether individuals or organizations, can share various types of content. This content can include informative articles, personal thoughts, or updates on a particular subject. These are presented in the form of articles or posts, typically written pieces accompanied by images or other multimedia elements.
+            </p>
             <a href="" class="btn btn-dark text-center">READ MORE</a>
         </div>
     </div>
@@ -33,23 +36,21 @@
         <div class="col bg-dark">
             <div class="row m-2 text-center">
                 <div class="col-2 m-3 text-center bg-light text-dark">
-                    <p>PHP</p>
+                    <p>Planning</p>
                 </div>
                 <div class="col-3 m-3 text-center bg-light text-dark">
-                    <p>Programming</p>
+                    <p>Content Creation</p>
                 </div>
                 <div class="col-2 m-3 text-center bg-light text-dark">
-                    <p>Languages</p>
+                    <p>Evaluate</p>
                 </div>
                 <div class="col-2 m-3 text-center bg-light text-dark">
-                    <p>Back-end</p>
+                    <p>Maintenance</p>
                 </div>
             </div>
             <div>
                 <p class="text-center text-start text-light p-3 container">            
-                    Made last it seen went no just when of by. Occasional entreaties comparison me difficulty so themselves. At brother inquiry of offices without do my service. As particular to companions at sentiments. Weather however luckily enquire so certain do. Aware did stood was day under ask. Dearest affixed enquire on explain opinion he. Reached who the mrs joy offices pleased. Towards did colonel article any parties.
-                    Consider now provided laughter boy landlord dashwood. Often voice and the spoke. No shewing fertile village equally prepare up females as an. That do an case an what plan hour of paid. Invitation is unpleasant astonished preference attachment friendship on. Did sentiments increasing particular nay. Mr he recurred received prospect in. Wishing cheered parlors adapted am at amongst matters.
-                    Difficulty on insensible reasonable in. From as went he they. Preference themselves me as thoroughly partiality considered on in estimating. Middletons acceptance discovered projecting so is so or. In or attachment inquietude remarkably comparison at an. Is surrounded prosperous stimulated am me discretion expression. But truth being state can she china widow. Occasional preference fat remarkably now projecting uncommonly dissimilar. Sentiments projection particular companions interested do at my delightful. Listening newspaper in advantage frankness to concluded.
+                    The blog project undergoes a systematic progression through key stages. In the planning phase, foundational decisions are made, outlining the purpose, target audience, and content strategy. Content creation then takes center stage, involving the generation of engaging and relevant material in the form of articles or posts. The evaluation step follows, where the project's performance is assessed through metrics like website traffic and user engagement, offering insights for refinement. Finally, maintenance becomes an ongoing task, ensuring the blog's longevity and relevance by updating content, fixing issues, and preserving a user-friendly experience. These stages collectively form a dynamic cycle, allowing for continuous improvement and adaptation to evolving goals and audience needs.
                 </p>
                 <a href="" class="btn btn-dark border-light text-center m-4">READ MORE</a>
             </div>
